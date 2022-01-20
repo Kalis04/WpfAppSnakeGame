d365377a8aeaa593b1d27cdbca6abd05ef5f51bf
Kristaps Kālis <kristaps.kalis04@gmail.com>
2022-01-20 19:31:45 +0200

    all good g

---
+++ WpfAppSnakeGame/SnakePart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+
+namespace WpfAppSnakeGame
+{
+    public class SnakePart
+    {
+        public UIElement UiElement { get; set; }
+
+        public Point Position { get; set; }
+
+        public bool IsHead { get; set; }
+    }
+}
